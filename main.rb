2f64cfec0a1bc884e536a9237ec9bf67bdfb1a5f
sueta1995 <39643866+sueta1995@users.noreply.github.com>
2022-10-23 00:59:33 +0300

    Update main.rb

--- main.rb
+++ main.rb
@@ -2,7 +2,7 @@
 
 require './maker'
 
-File.open('script.txt', 'w') do |f|
+File.open('script.sql', 'w') do |f|
   f.puts('INSERT INTO assignments (laboriousness, issue_date, planned_expiration_date, real_expiration_date, project_id, employee_id) VALUES')
   1000.times { f.puts(Maker.new.create) }
 end
